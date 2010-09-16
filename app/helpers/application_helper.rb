f97aad8445bd087daed42d7d5a3b5ea68bda6ed4
quazletepl <quazletepl@gmail.com>
2010-09-16 11:16:48 +0200

    layout refined

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+	def logo
+	logo = image_tag("rails.png", :alt => "PBZ Totto", :class => "round")
+	end
+
 end
